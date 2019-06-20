d31436c575c3dbf0f133d029910246162a3e3d06
hopunchen <hopun.c@gmail.com>
2019-06-20 20:32:04 +0800

    add markdown

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-
+import markdown
 # Create your views here.
 from django.http import HttpResponse
 from .models import Post
@@ -10,4 +10,11 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
